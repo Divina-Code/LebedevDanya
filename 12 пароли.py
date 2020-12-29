2dedf4f86c4c1e010fe0e3113eae12f357faec8d
keno147 <76617894+keno147@users.noreply.github.com>
2020-12-29 18:39:45 +0300

    Update 12 пароли.py

--- 12 пароли.py
+++ 12 пароли.py
@@ -2,18 +2,31 @@ from random import choice, randint
 letters = str('abcdefg')
 symb = str('!@#$%^&*')
 numbers = str('0123456789')
+let1 = 0 #кол-во букв
+sym1 = 0 #кол-во символов
+num1 = 0#кол-во цифр
+passed = False # порверка пароля на подходяшность
 
-password = ('')
-for a in range(8):
-    b = randint(1,3)
-    if b == 1:
-        c = choice(letters)
-        d = randint(1,2)
-        if d == 1:
-            c=c.upper()
-        password += c
-    if b == 2:
-        password += choice(symb)
-    if b == 3:
-        password += choice(numbers)
+while not passed:
+    password = ('')
+    for a in range(8):
+        b = randint(1,3) #выбор из цифр, букв и символов
+        if b == 1: # буквы
+            c = choice(letters)
+            d = randint(1,2)
+            if d == 1:
+                c=c.upper()
+            password += c
+        elif b == 2: # сиволы
+            password += choice(symb)
+        elif b == 3:# цифры
+            password += choice(numbers)
+        if b == 1:
+            let1 += 1
+        elif b == 2:
+            sym1 += 1
+        elif b == 3:
+            num1 += 1
+        if let1>0 and sym1>0 and num1>0:
+            passed=True #проверка пароля на подходяшность
 print (password)
